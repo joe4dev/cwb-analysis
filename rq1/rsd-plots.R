1646403b588f2cbeb6e04cab81e6895afab4f8fa
Joel Scheuner <joel.scheuner.dev@gmail.com>
2017-05-22 19:38:47 +0200

    Specify input directory because the script path detection doesn't work in CLI

--- rq1/rsd-plots.R
+++ rq1/rsd-plots.R
@@ -4,10 +4,10 @@ library(reshape2)
 library(ggplot2)
 
 ### BEGIN CONFIGUARION ###
-script.dir <- dirname(sys.frame(1)$ofile)
+# script.dir <- dirname(sys.frame(1)$ofile) # doesn't work in CLI
 
 # Input directory where the input CSV is located
-src.dir <- script.dir # '~/Projects/CloudWorkBench/cwb-analysis/rq1'
+src.dir <- '~/Projects/CloudWorkBench/cwb-analysis/rq1' # script.dir
 src.file_name <- 'cwb-interim-aggregated-selected-filtered.csv'
 
 # Output directory where the resulting PDF will be saved
